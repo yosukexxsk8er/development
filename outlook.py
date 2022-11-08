b588fdffc93cef5e9e1f5e0f6e1578bf8d080f48
unknown <yosukexxsk8er@gmail.com>
2022-11-09 01:22:22 +0900

    AAA

---
+++ outlook.py
@@ -0,0 +1,25 @@
+import win32com.client
+import datetime
+
+outlook = win32com.client.Dispatch("outlook.Application")
+
+print(type(outlook))
+
+
+APPOINTMENT_ITEM = 1
+item = outlook.CreateItem(APPOINTMENT_ITEM)
+a = datetime.datetime(2022, 11, 12, 13,15)
+b =datetime.datetime.today()
+print(type(a))
+print(type(b))
+item.Start = a
+item.Duration = 30
+item.Subject = '定例ミーティング'
+item.Body = 'いつものミーティング'
+item.ReminderMinutesBeforeStart = 0
+item.ReminderSet = True
+item.Save()
+
+
+dt_now = datetime.datetime.now()
+print(dt_now)
