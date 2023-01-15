f027e64f555c7c097bc1d9a7b2f899cbdd467906
unknown <yosukexxsk8er@gmail.com>
2023-01-16 01:36:20 +0900

    Updated

--- md2html.py
+++ md2html.py
@@ -14,9 +14,8 @@ def main():
     table = [["xxxx", "yyyy"], ["zzzz", "vvvv"]]
 
     html =  ""
-    html += style("td", border="solid 0px")
-    html += style("th", border="solid 0px")
-
+    html += StyleProperty(border="solid 0px").header("td")
+    html += StyleProperty(border="solid 0px").header("th")
 
     table_style = StyleProperty(font_size="large", background_color="yellow")
 
@@ -64,7 +63,7 @@ class StyleProperty:
     #色=None・背景	適用対象	継承
     color:str=None                 # 文字色	全ての要素	する
     background:str=None            # 背景関連をまとめて指定	全ての要素	しない
-    background_color:str="transparent"      # 背景色	全ての要素	しない
+    background_color:str=None      # 背景色	全ての要素	しない
     background_image:str=None      # 背景画像	全ての要素	しない
     background_repeat:str=None     # 背景画像の並び方	全ての要素	しない
     background_position:str=None   # 背景画像の表示位置	ブロックレベル要素、置換要素	しない
@@ -135,91 +134,35 @@ class StyleProperty:
     empty_cells:str=None     # 空セルの境界線	th要素、td要素	する
     caption_side:str=None    # 表タイトルの位置	caption要素	する
 
+    def has_valid(self):
+        for value in vars(self).values():
+            if(value is not None):
+                return True
+        return False
+
     def add(self):
-        style  = ' style="'
-        style += f'font:{self.font};'                                  if(self.font)                 else ""
-        style += f'font-size:{self.font_size};'                        if(self.font_size)            else ""
-        style += f'font-weight:{self.font_weight};'                    if(self.font_weight)          else ""
-        style += f'font-style:{self.font_style};'                      if(self.font_style)           else ""
-        style += f'font-family:{self.font_family};'                    if(self.font_family)          else ""
-        style += f'font-variant:{self.font_variant};'                  if(self.font_variant)         else ""
-        style += f'text-align:{self.text_align};'                      if(self.text_align)           else ""
-        style += f'vertical-align:{self.vertical_align};'              if(self.vertical_align)       else ""
-        style += f'line-height:{self.line_height};'                    if(self.line_height)          else ""
-        style += f'text-decoration:{self.text_decoration};'            if(self.text_decoration)      else ""
-        style += f'text-indent:{self.text_indent};'                    if(self.text_indent)          else ""
-        style += f'text-transform:{self.text_transform};'              if(self.text_transform)       else ""
-        style += f'letter-spacing:{self.letter_spacing};'              if(self.letter_spacing)       else ""
-        style += f'word-spacing:{self.word_spacing};'                  if(self.word_spacing)         else ""
-        style += f'white-space:{self.white_space};'                    if(self.white_space)          else ""
-        style += f'color:{self.color};'                                if(self.color)                else ""
-        style += f'background:{self.background};'                      if(self.background)           else ""
-        style += f'background-color:{self.background_color};'          if(self.background_color)     else ""
-        style += f'background-image:{self.background_image};'          if(self.background_image)     else ""
-        style += f'background-repeat:{self.background_repeat};'        if(self.background_repeat)    else ""
-        style += f'background-position:{self.background_position};'    if(self.background_position)  else ""
-        style += f'background-attachment:{self.background_attachment};' if(self.background_attachment) else ""
-        style += f'width:{self.width};'                                if(self.width)                else ""
-        style += f'height:{self.height};'                              if(self.height)               else ""
-        style += f'max-width:{self.max_width};'                        if(self.max_width)            else ""
-        style += f'min-width:{self.min_width};'                        if(self.min_width)            else ""
-        style += f'max-height:{self.max_height};'                      if(self.max_height)           else ""
-        style += f'min-height:{self.min_height};'                      if(self.min_height)           else ""
-        style += f'margin:{self.margin};'                              if(self.margin)               else ""
-        style += f'm-argin-top:{self.m_argin_top};'                    if(self.m_argin_top)          else ""
-        style += f'm-argin-right:{self.m_argin_right};'                if(self.m_argin_right)        else ""
-        style += f'm-argin-bottom:{self.m_argin_bottom};'              if(self.m_argin_bottom)       else ""
-        style += f'm-argin-left:{self.m_argin_left};'                  if(self.m_argin_left)         else ""
-        style += f'p-adding:{self.p_adding};'                          if(self.p_adding)             else ""
-        style += f'p-adding-top:{self.p_adding_top};'                  if(self.p_adding_top)         else ""
-        style += f'p-adding-right:{self.p_adding_right};'              if(self.p_adding_right)       else ""
-        style += f'p-adding-bottom:{self.p_adding_bottom};'            if(self.p_adding_bottom)      else ""
-        style += f'p-adding-left:{self.p_adding_left};'                if(self.p_adding_left)        else ""
-        style += f'border:{self.border};'                              if(self.border)               else ""
-        style += f'border-top:{self.border_top};'                      if(self.border_top)           else ""
-        style += f'border-right:{self.border_right};'                  if(self.border_right)         else ""
-        style += f'border-bottom:{self.border_bottom};'                if(self.border_bottom)        else ""
-        style += f'border-left:{self.border_left};'                    if(self.border_left)          else ""
-        style += f'border-width:{self.border_width};'                  if(self.border_width)         else ""
-        style += f'border-top-width:{self.border_top_width};'          if(self.border_top_width)     else ""
-        style += f'border-right-width:{self.border_right_width};'      if(self.border_right_width)   else ""
-        style += f'border-bottom-width:{self.border_bottom_width};'    if(self.border_bottom_width)  else ""
-        style += f'border-left-width:{self.border_left_width};'        if(self.border_left_width)    else ""
-        style += f'border-color:{self.border_color};'                  if(self.border_color)         else ""
-        style += f'border-top-color:{self.border_top_color};'          if(self.border_top_color)     else ""
-        style += f'border-right-color:{self.border_right_color};'      if(self.border_right_color)   else ""
-        style += f'border-bottom-color:{self.border_bottom_color};'    if(self.border_bottom_color)  else ""
-        style += f'border-left-color:{self.border_left_color};'        if(self.border_left_color)    else ""
-        style += f'border-style:{self.border_style};'                  if(self.border_style)         else ""
-        style += f'border-top-style:{self.border_top_style};'          if(self.border_top_style)     else ""
-        style += f'border-right-style:{self.border_right_style};'      if(self.border_right_style)   else ""
-        style += f'border-bottom-style:{self.border_bottom_style};'    if(self.border_bottom_style)  else ""
-        style += f'border-left-style:{self.border_left_style};'        if(self.border_left_style)    else ""
-        style += f'overflow:{self.overflow};'                          if(self.overflow)             else ""
-        style += f'display:{self.display};'                            if(self.display)              else ""
-        style += f'visibility:{self.visibility};'                      if(self.visibility)           else ""
-        style += f'clip:{self.clip};'                                  if(self.clip)                 else ""
-        style += f'float:{self.float};'                                if(self.float)                else ""
-        style += f'clear:{self.clear};'                                if(self.clear)                else ""
-        style += f'position:{self.position};'                          if(self.position)             else ""
-        style += f'top:{self.top};'                                    if(self.top)                  else ""
-        style += f'right:{self.right};'                                if(self.right)                else ""
-        style += f'bottom:{self.bottom};'                              if(self.bottom)               else ""
-        style += f'left:{self.left};'                                  if(self.left)                 else ""
-        style += f'z-index:{self.z_index};'                            if(self.z_index)              else ""
-        style += f'direction:{self.direction};'                        if(self.direction)            else ""
-        style += f'unicode-bidi:{self.unicode_bidi};'                  if(self.unicode_bidi)         else ""
-        style += f'list-style:{self.list_style};'                      if(self.list_style)           else ""
-        style += f'list-style-type:{self.list_style_type};'            if(self.list_style_type)      else ""
-        style += f'list-style-position:{self.list_style_position};'    if(self.list_style_position)  else ""
-        style += f'list-style-image:{self.list_style_image};'          if(self.list_style_image)     else ""
-        style += f'table-layout:{self.table_layout};'                  if(self.table_layout)         else ""
-        style += f'border-collapse:{self.border_collapse};'            if(self.border_collapse)      else ""
-        style += f'border-spacing:{self.border_spacing};'              if(self.border_spacing)       else ""
-        style += f'empty-cells:{self.empty_cells};'                    if(self.empty_cells)          else ""
-        style += f'caption-side:{self.caption_side};'                  if(self.caption_side)         else ""
-        style += '"'
-        return style
+        if(self.has_valid()):
+            style = ' style="'
+            for key, value in vars(self).items():
+                if(value is not None):
+                    style+= f'{key.replace("_", "-")}:{value};'
+
+            style+= '"'
+            return style
+        else:
+            return ""
+
+    def header(self, tag, indent="    "):
+        style = []
+        style.append(f'<style>')
+        style.append(f'{indent * 1}{tag} {{')
+        for key, value in vars(self).items():
+            if(value is not None):
+                style.append(f'{indent * 2}{key.replace("_", "-")}:{value};')
+        #style.append(f'{indent * 2}border: {border};')
+        style.append(f'{indent * 1}}}')
+        style.append(f'</style>')
+        return "\n".join(style) + "\n"
 
 
 def style(tag, border="sold 1px", bg="#cdefff", indent="    "):
