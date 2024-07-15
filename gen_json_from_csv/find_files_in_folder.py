8e64a56e9326dec3a510225be73025de585d83e5
Your Name <you@example.com>
2024-07-16 08:38:14 +0900

    modified:   gen_json_from_csv/find_files_in_folder.py modified:   gen_json_from_csv/merge_json_horizon.py

--- gen_json_from_csv/find_files_in_folder.py
+++ gen_json_from_csv/find_files_in_folder.py
@@ -1,13 +1,15 @@
 import os
+import re
+
 def find_files_in_folder(folder, pattern, extension):
     """
     Find files in a folder that match a specific pattern and extension.
     
     Args:
         folder (str): The folder path to search in.
-        pattern (str): The specific pattern to match in the file names.
+        pattern (str): The specific pattern (regular expression) to match in the file names.
         extension (str): The specific extension to match in the file names.
-    
+        
     Returns:
         list: A list of file paths that match the pattern and extension.
     """
@@ -15,7 +17,7 @@ def find_files_in_folder(folder, pattern, extension):
     
     for root, dirs, files in os.walk(folder):
         for file in files:
-            if pattern in file and file.endswith(extension):
+            if re.search(pattern, file) and file.endswith(extension):
                 matched_files.append(os.path.join(root, file))
     
     return matched_files
--- gen_json_from_csv/merge_json_horizon.py
+++ gen_json_from_csv/merge_json_horizon.py
@@ -1,14 +1,41 @@
 
 from gen_json_from_csv.find_files_in_folder import find_files_in_folder
+import json
+import re
+import os
 
 def main():
     pass
 
     folder = r"logs\20240714A"
-    pattern = "01"
+    pattern = "^\d+_"
     extension = ".json"
+    output_dir = "merged"
+    os.makedirs(output_dir, exist_ok=True)
+
     file_list = find_files_in_folder(folder=folder, pattern=pattern, extension=extension)
+    mapped_dict = {}
+    type_order = ["setting", "result"]
+    for type in type_order:
+        for file in file_list:
+            m = re.search(rf"^(\d+)_.*{type}.*\.json", os.path.basename(file))
+            if(m):
+                try:
+                    mapped_dict[m.group(1)].append(file)
+                except KeyError:
+                    mapped_dict[m.group(1)] = [file]
+
+
     pass
+
+    for key, mapped_file_list in mapped_dict.items():
+        merged_dict = {}
+        for file in mapped_file_list:
+            with open(file) as f:
+                merged_dict.update(json.load(f))
+        with open(f"{output_dir}/merged_{key}.json", "w") as f:
+            json.dump(merged_dict, f, indent=4)
+
 
 
 
